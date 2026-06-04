2943c1044b6451c82bd273bff2652056e4f19e43
simonabaldovino <baldovinosimona020@gmail.com>
2026-06-04 10:38:12 -0300

    nuevo frm editar clientes

---
+++ pryGestionClientesBdD/frmEditarCliente.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace pryGestionClientesBdD
+{
+    partial class frmEditarCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblLimite = new System.Windows.Forms.Label();
+            this.lblDeuda = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblNom = new System.Windows.Forms.Label();
+            this.lblLimiteCred = new System.Windows.Forms.Label();
+            this.lblDeud = new System.Windows.Forms.Label();
+            this.btnÇEliminar = new System.Windows.Forms.Button();
+            this.lblGuardar = new System.Windows.Forms.Button();
+            this.lblModificar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(27, 32);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(91, 13);
+            this.lblCodigo.TabIndex = 0;
+            this.lblCodigo.Text = "Código del cliente";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(175, 29);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(130, 20);
+            this.txtCodigo.TabIndex = 1;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(175, 67);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(130, 23);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblLimite);
+            this.groupBox1.Controls.Add(this.lblDeuda);
+            this.groupBox1.Controls.Add(this.lblNombre);
+            this.groupBox1.Controls.Add(this.lblNom);
+            this.groupBox1.Controls.Add(this.lblLimiteCred);
+            this.groupBox1.Controls.Add(this.lblDeud);
+            this.groupBox1.Location = new System.Drawing.Point(18, 110);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(287, 184);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos del Cliente";
+            // 
+            // lblLimite
+            // 
+            this.lblLimite.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblLimite.Location = new System.Drawing.Point(128, 136);
+            this.lblLimite.Name = "lblLimite";
+            this.lblLimite.Size = new System.Drawing.Size(100, 23);
+            this.lblLimite.TabIndex = 8;
+            // 
+            // lblDeuda
+            // 
+            this.lblDeuda.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblDeuda.Location = new System.Drawing.Point(126, 92);
+            this.lblDeuda.Name = "lblDeuda";
+            this.lblDeuda.Size = new System.Drawing.Size(100, 23);
+            this.lblDeuda.TabIndex = 7;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblNombre.Location = new System.Drawing.Point(128, 44);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(100, 23);
+            this.lblNombre.TabIndex = 6;
+            // 
+            // lblNom
+            // 
+            this.lblNom.AutoSize = true;
+            this.lblNom.Location = new System.Drawing.Point(29, 44);
+            this.lblNom.Name = "lblNom";
+            this.lblNom.Size = new System.Drawing.Size(44, 13);
+            this.lblNom.TabIndex = 4;
+            this.lblNom.Text = "Nombre";
+            // 
+            // lblLimiteCred
+            // 
+            this.lblLimiteCred.AutoSize = true;
+            this.lblLimiteCred.Location = new System.Drawing.Point(29, 146);
+            this.lblLimiteCred.Name = "lblLimiteCred";
+            this.lblLimiteCred.Size = new System.Drawing.Size(89, 13);
+            this.lblLimiteCred.TabIndex = 5;
+            this.lblLimiteCred.Text = "Límite de crédito ";
+            // 
+            // lblDeud
+            // 
+            this.lblDeud.AutoSize = true;
+            this.lblDeud.Location = new System.Drawing.Point(29, 93);
+            this.lblDeud.Name = "lblDeud";
+            this.lblDeud.Size = new System.Drawing.Size(39, 13);
+            this.lblDeud.TabIndex = 3;
+            this.lblDeud.Text = "Deuda";
+            // 
+            // btnÇEliminar
+            // 
+            this.btnÇEliminar.Location = new System.Drawing.Point(18, 313);
+            this.btnÇEliminar.Name = "btnÇEliminar";
+            this.btnÇEliminar.Size = new System.Drawing.Size(87, 23);
+            this.btnÇEliminar.TabIndex = 8;
+            this.btnÇEliminar.Text = "Eliminar";
+            this.btnÇEliminar.UseVisualStyleBackColor = true;
+            // 
+            // lblGuardar
+            // 
+            this.lblGuardar.Location = new System.Drawing.Point(218, 313);
+            this.lblGuardar.Name = "lblGuardar";
+            this.lblGuardar.Size = new System.Drawing.Size(87, 23);
+            this.lblGuardar.TabIndex = 9;
+            this.lblGuardar.Text = "Guardar";
+            this.lblGuardar.UseVisualStyleBackColor = true;
+            // 
+            // lblModificar
+            // 
+            this.lblModificar.Location = new System.Drawing.Point(116, 313);
+            this.lblModificar.Name = "lblModificar";
+            this.lblModificar.Size = new System.Drawing.Size(87, 23);
+            this.lblModificar.TabIndex = 10;
+            this.lblModificar.Text = "Modificar";
+            this.lblModificar.UseVisualStyleBackColor = true;
+            // 
+            // frmEditarCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 348);
+            this.Controls.Add(this.lblModificar);
+            this.Controls.Add(this.btnÇEliminar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblGuardar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.lblCodigo);
+            this.Name = "frmEditarCliente";
+            this.Text = "frmEditarCliente";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblLimite;
+        private System.Windows.Forms.Label lblDeuda;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblNom;
+        private System.Windows.Forms.Label lblLimiteCred;
+        private System.Windows.Forms.Label lblDeud;
+        private System.Windows.Forms.Button btnÇEliminar;
+        private System.Windows.Forms.Button lblGuardar;
+        private System.Windows.Forms.Button lblModificar;
+    }
+}
